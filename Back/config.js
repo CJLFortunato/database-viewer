5b6cad5424252027f3524e8f0810e362560e27a0
CJL Fortunato <clemence.fortunato@gmail.com>
2022-04-21 10:31:24 +0200

    Still trying things

--- Back/config.js
+++ Back/config.js
@@ -1,12 +1,13 @@
 const Pool = require('pg').Pool;
 
-const connexionString = process.env.DATABASE_URL + '&sslmode=require';
+const connexionString = process.env.DATABASE_URL;
 
 console.log(connexionString);
 
 const pool = new Pool({
   connexionString: connexionString,
   ssl: {
+    required: true,
     rejectUnauthorized: false,
   }
 });
