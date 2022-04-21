88671b05a68dbf814bca59826c03d0cd907daeb7
CJL Fortunato <clemence.fortunato@gmail.com>
2022-04-21 10:04:36 +0200

    Still trying things

--- Back/config.js
+++ Back/config.js
@@ -1,6 +1,9 @@
 const Pool = require('pg').Pool;
 
 const connexionString = process.env.DATABASE_URL;
+
+console.log(connexionString);
+
 const pool = new Pool({
   connexionString: connexionString,
   ssl: {
